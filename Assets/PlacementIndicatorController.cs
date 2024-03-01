5b72d1f6a591039a63f9eeb0602769cd6de5f6f5
Livia Michelle Iskandar Leo <e0851445@nus.edu.sg>
2024-03-01 18:45:08 +0800

    Add Furniture UIs, Configure Buttons

---
+++ Assets/PlacementIndicatorController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementIndicatorController : MonoBehaviour
+{
+
+    public GameObject placementIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        placementIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
