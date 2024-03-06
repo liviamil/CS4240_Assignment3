6a805762022b83af100c298071f860d5a0d10395
weenahh <wnshta.school@gmail.com>
2024-03-06 16:36:44 +0100

    Created a select button
    To replace the issue of accidental taps on the screen. But still working on making it spawn, there is something missing

--- Assets/Scripts/ARTapToPlaceObject.cs
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -2,10 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
     public GameObject placementIndicator;
+    public Button tapToPlaceButton; // Reference to the TapToPlace button
     private GameObject objToSpawn;
 
     private Pose PlacementPose; 
@@ -15,27 +17,22 @@ public class ARTapToPlaceObject : MonoBehaviour
     private void Start()
     {
         raycastManager = FindObjectOfType<ARRaycastManager>();
+
+        // Add an event listener to the TapToPlace button
+        tapToPlaceButton.onClick.AddListener(PlaceObject);
     }
 
     private void Update()
     {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
-        // if there is a valid location + we tap the screen, spawn an item at that location
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            PlaceObject();
-        }
     }
 
     void UpdatePlacementPose()
     {
-        // convert viewport position to screen position. Center of screen may not be (0.5, 0.5) since different phones have different sizes
         var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f)); 
-        // shoot a ray out from middle of screen to see if it hits anything
         var hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
-        // is there a plane and are we currently facing it
         placementPoseIsValid = hits.Count > 0;
         if (placementPoseIsValid)
         {
@@ -43,20 +40,15 @@ public class ARTapToPlaceObject : MonoBehaviour
         }
     }
 
-    /**
-     * Move the placement indicator object
-     */
     void UpdatePlacementIndicator()
     {
         if (placementPoseIsValid)
         {
-            // if there is a valid plane, activate placement indicator object and make it follow around
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
         }
         else
         {
-            // no valid place, deactivate
             placementIndicator.SetActive(false);
         }
     }
@@ -68,6 +60,10 @@ public class ARTapToPlaceObject : MonoBehaviour
 
     private void PlaceObject()
     {
-        Instantiate(objToSpawn, PlacementPose.position, PlacementPose.rotation);
+        // Check if the tap occurs within the bounds of the button
+        if (EventSystem.current.currentSelectedGameObject == tapToPlaceButton.gameObject)
+        {
+            Instantiate(objToSpawn, PlacementPose.position, PlacementPose.rotation);
+        }
     }
 }
