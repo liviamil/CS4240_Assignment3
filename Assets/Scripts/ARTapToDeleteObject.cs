d11c92d9b25aa7a2c08d514344f63f99161a7b8a
weenahh <wnshta.school@gmail.com>
2024-03-11 07:40:30 +0100

    Delete works but only sometimes

--- Assets/Scripts/ARTapToDeleteObject.cs
+++ Assets/Scripts/ARTapToDeleteObject.cs
@@ -8,86 +8,74 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ARTapToDeleteObject : MonoBehaviour
 {
-    public GameObject placementIndicator;
-    private GameObject hitObject; // Store reference to the object to delete
-
-    private Pose PlacementPose;
     public ARRaycastManager raycastManager;
-    private bool placementPoseIsValid = false;
+    private GameObject objToDelete;
+    private bool collisionDetected = false;
+
+    public GameObject placementIndicator;
+    private Pose placementPose;
+
 
 
     private void Start()
     {
         raycastManager = FindObjectOfType<ARRaycastManager>();
+        placementIndicator.SetActive(false); // Ensure the placement indicator is initially hidden
     }
 
     public void OnTapButtonClick()
     {
-        // If there is a valid location and a valid hit object, delete it
-        if (placementPoseIsValid && hitObject != null)
+        if (collisionDetected && objToDelete != null)
         {
             DeleteObject();
-        }        
+        }
     }
 
     private void Update()
     {
-        UpdatePlacementPose();
         UpdatePlacementIndicator();
+        UpdateCollisionStatus();
     }
 
-    void UpdatePlacementPose()
+
+    void UpdatePlacementIndicator()
     {
         // convert viewport position to screen position. Center of screen may not be (0.5, 0.5) since different phones have different sizes
-        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        
-        // shoot a ray out from middle of screen to see if it hits anything
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f)); 
         var hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
 
-        // is there a plane and are we currently facing it
-        placementPoseIsValid = hits.Count > 0;
-        if (placementPoseIsValid)
+        if (hits.Count > 0)
         {
-            PlacementPose = hits[0].pose;
-
-            // Check for collisions with existing objects
-            Collider[] colliders = Physics.OverlapBox(PlacementPose.position, placementIndicator.GetComponent<BoxCollider>().size / 2f, Quaternion.identity);
-            bool objectFound = false;
-            foreach (Collider collider in colliders)
-            {
-                if (collider.CompareTag("ARObject"))
-                {
-                    hitObject = collider.gameObject;
-                    objectFound = true;
-                    break;
-                }
-            }
-
-            if (!objectFound)
-            {
-                hitObject = null;
-            }
+            placementPose = hits[0].pose;
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         }
         else
         {
-            hitObject = null;
+            placementIndicator.SetActive(false);
         }
     }
-    
-    void UpdatePlacementIndicator()
+
+    void UpdateCollisionStatus()
     {
-        placementIndicator.SetActive(placementPoseIsValid);
-        if (placementPoseIsValid)
+        collisionDetected = false; // Reset collision status
+
+        // Perform a raycast from the placement indicator upwards to detect objects
+        if (Physics.Raycast(placementPose.position, Vector3.up, out RaycastHit hit, Mathf.Infinity))
         {
-            // Set placement indicator's position to the camera's position and rotation
-            placementIndicator.transform.SetPositionAndRotation(Camera.main.transform.position, Camera.main.transform.rotation);
+            if (hit.collider.CompareTag("ARObject"))
+            {
+                collisionDetected = true;
+                objToDelete = hit.collider.gameObject;
+            }
         }
     }
+
 
     void DeleteObject()
     {
         // Delete the object
-        Destroy(hitObject);
+        Destroy(objToDelete);
     }
 }
